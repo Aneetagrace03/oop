a2ef368684ed16bd798024d37ddeeaae4e5a9ec1
Aneetagrace03 <aneetagraceabraham2028@cs.sjcetpalai.ac.in>
2025-07-19 22:00:40 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Reverse{
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the number");
+        int number=sc.nextInt();
+        int reverse=0;
+        while(number!=0)
+{      int digit=number%10;
+       reverse=reverse*10+digit;
+       number=number/10;
+}    System.out.println("reversed no ="+reverse);
+    
+    
+    
+    
+    }}
