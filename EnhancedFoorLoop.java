4fa453bdfd712398c64b3367c96a3e449f16b748
Aneetagrace03 <aneetagraceabraham2028@cs.sjcetpalai.ac.in>
2025-07-19 09:15:28 +0530

    Add files via upload

---
+++ EnhancedFoorLoop.java
@@ -0,0 +1,10 @@
+class EnhancedFoorLoop{
+    public static void main(String[] args)
+    {
+        int arr[]={1,235,67,96,33,6,7,5};
+        for(int i:arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
